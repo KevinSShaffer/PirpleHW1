a06949ef041023b784e8416ade84cd27a10f392d
Kevin Shaffer <kshaffer@scramsystems.com>
2018-08-14 09:42:20 -0600

    Implemented Router

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    const query = parsedUrl.query;
-    const method = req.method.toUpperCase();
-    const headesr = req.headers;
     const decoder = new StringDecoder('utf-8');
     let buffer = '';
 
@@ -19,9 +16,32 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
         buffer += decoder.end();
 
-        res.end('Hello World!\n');
-    
-        console.log(`Request received with data: ${buffer}`);
+        let chosenHandler = router[trimmedPath];
+
+        if (chosenHandler == undefined)
+            chosenHandler = handlers.notFound;
+
+        const request = {
+            path: trimmedPath,
+            query: parsedUrl.query,
+            method: req.method.toUpperCase(),
+            headers: req.headers,
+            body: buffer
+        }
+
+        chosenHandler(request, (statusCode, payload) => {
+            if (typeof(statusCode) != 'number')
+                statusCode = 200;
+            if (typeof(payload) != 'object')
+                payload = {};
+            
+            const responseBody = JSON.stringify(payload);
+            
+            res.writeHead(statusCode);
+            res.end(responseBody);
+
+            console.log(`Returning this response: (${statusCode}) ${responseBody}`);
+        });
     });
 });
 
@@ -29,3 +49,15 @@ const port = 3000;
 server.listen(port, () => {
     console.log(`The server is listening on port ${port}`)
 });
+
+const handlers = {
+    sample(data, callback) {
+        callback(200, data)
+    },
+    notFound(data, callback) {
+        callback(404)
+    }
+}
+const router = {
+    sample: handlers.sample
+}
